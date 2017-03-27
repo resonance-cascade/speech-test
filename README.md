867f9ba96975243901bbeb03f7f67d52e0feb5fc
Bret Comnes <bcomnes@gmail.com>
2017-03-26 19:12:05 -0700

    progress

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # speech-test
-electron getUserMedia to google speech test
+
+## OUR OLD TEST
+
+The orignal test file we worked on lives in the `sox.js` file.
+
+Run with `npm run sox`.  Make sure you have `sox` installed on your system first.
+
+```sh
+$ brew install sox  # osx
+```
+
+## The new tests
+
+
--- sox_renderer.js
+++ sox_renderer.js
@@ -1,13 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const getUserMedia = require('getusermedia')
-const Speech = require('@google-cloud/speech')
-const path = require('path')
-const audio = require('audio-stream')
-const pcm = require('pcm-stream')
-const wave = require('./wave-stream')
 
+const record = require('node-record-lpcm16') // [START speech_streaming_mic_recognize]
+const Speech = require('@google-cloud/speech') // Imports the Google Cloud client library
+const path = require('path')
+    // const speech = Speech() // Instantiates a client
 const myPath = path.join(__dirname, '/auth.json')
     // adjust_path_to_os(__dirname + '/dexter-dev-env-b05da431ead6.json')
 
@@ -33,25 +31,8 @@ const recognizeStream = speech.createRecognizeStream(request)
               // process.stdout.write(data.results)
 
     // Start recording and send the microphone input to the Speech API
-
-getUserMedia({video: false, audio: true}, function (err, mediaStream) {
-  if (err) return console.error(err)
-  var w = wave()
-  var sourceStream = audio(mediaStream)
-  sourceStream
-        .on('header', function (header) {
-          var channels = header.channels
-          var sampleRate = header.sampleRate
-          w.setHeader({
-            audioFormat: 1,
-            channels: channels,
-            sampleRate: sampleRate,
-            byteRate: sampleRate * channels * 2,
-            blockAlign: channels * 2,
-            bitDepth: 16
-          })
-        })
-        .pipe(pcm())
-        .pipe(w)
-        .pipe(recognizeStream)
-})
+record.start({
+  sampleRate: 16000,
+  threshold: 0
+}).pipe(recognizeStream)
+console.log('Listening, press Ctrl+C to stop.')
